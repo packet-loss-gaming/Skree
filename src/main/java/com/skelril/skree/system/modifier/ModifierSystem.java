e1c18f476a761757f750124421f214e26888a556
Wyatt Childers <wchilders@nearce.com>
2015-05-10 15:57:50 -0400

    Register the Modifier Service

--- src/main/java/com/skelril/skree/system/modifier/ModifierSystem.java
+++ src/main/java/com/skelril/skree/system/modifier/ModifierSystem.java
@@ -6,5 +6,27 @@
 
 package com.skelril.skree.system.modifier;
 
+import com.google.inject.Inject;
+import com.skelril.skree.SkreePlugin;
+import com.skelril.skree.service.ModifierService;
+import com.skelril.skree.service.internal.modifier.ModifierServiceImpl;
+import org.spongepowered.api.Game;
+import org.spongepowered.api.service.ProviderExistsException;
+
 public class ModifierSystem {
+
+    private ModifierService service = new ModifierServiceImpl();
+
+    @Inject
+    public ModifierSystem(SkreePlugin plugin, Game game) {
+        try {
+            game.getServiceManager().setProvider(plugin, ModifierService.class, service);
+        } catch (ProviderExistsException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ModifierService getService() {
+        return service;
+    }
 }
