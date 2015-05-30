39b1d1dcccddfa004154479771631bb3826ca4e0
Wyatt Childers <wchilders@nearce.com>
2015-05-29 21:34:15 -0400

    More thourough implementation of the Luminositor

--- src/main/java/com/skelril/skree/content/registry/item/generic/Luminositor.java
+++ src/main/java/com/skelril/skree/content/registry/item/generic/Luminositor.java
@@ -6,7 +6,6 @@
 
 package com.skelril.skree.content.registry.item.generic;
 
-import com.flowpowered.math.vector.Vector3d;
 import com.google.common.base.Optional;
 import com.skelril.nitro.registry.item.CustomItem;
 import net.minecraft.creativetab.CreativeTabs;
@@ -16,7 +15,11 @@ import org.spongepowered.api.entity.player.Player;
 import org.spongepowered.api.event.Subscribe;
 import org.spongepowered.api.event.entity.player.PlayerInteractBlockEvent;
 import org.spongepowered.api.item.inventory.ItemStack;
-import org.spongepowered.api.text.chat.ChatTypes;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.Texts;
+import org.spongepowered.api.text.format.TextColor;
+import org.spongepowered.api.text.format.TextColors;
+import org.spongepowered.api.world.Location;
 
 public class Luminositor extends Item implements CustomItem {
 
@@ -33,15 +36,36 @@ public class Luminositor extends Item implements CustomItem {
     @Subscribe
     public void onRightClick(PlayerInteractBlockEvent event) {
         // TODO needs right click support & light level fetching support
-        if (event.getInteractionType() == EntityInteractionTypes.USE && false) {
+        if (event.getInteractionType() == EntityInteractionTypes.USE) {
             Player player = event.getEntity();
-            Optional<Vector3d> optClickedPosition = event.getClickedPosition();
+            //Optional<Vector3d> optClickedPosition = event.getClickedPosition();
             Optional<ItemStack> optHeldItem = player.getItemInHand();
 
-            if (optHeldItem.isPresent() && optClickedPosition.isPresent()) {
+            if (optHeldItem.isPresent() /* && optClickedPosition.isPresent() */) {
                 if (this.equals(optHeldItem.get().getItem())) {
-                    int luminance = event.getBlock().add(optClickedPosition.get()).getLuminance();
-                    player.sendMessage(ChatTypes.ACTION_BAR, "Light level: " + luminance);
+                    Location pLoc = player.getLocation();
+
+                    int lightLevel = pLoc.getLuminance();
+
+                    TextColor color;
+                    if (lightLevel >= 12) {
+                        color = TextColors.GREEN;
+                    } else if (lightLevel >= 8) {
+                        color = TextColors.RED;
+                    } else {
+                        color = TextColors.DARK_RED;
+                    }
+
+                    Text message = Texts.builder().color(TextColors.YELLOW).append(
+                            Texts.of("Light level: ")
+                    ).build();
+                    // TODO system message.color(color)
+                    player.sendMessage(
+                            Texts.of(
+                                    message,
+                                    Texts.builder().color(color).append(Texts.of(lightLevel)).build()
+                            )
+                    );
                 }
             }
         }
