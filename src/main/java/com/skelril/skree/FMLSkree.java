5d38dee0fb7d172c8ddcb27699cf5f66e55f1512
Wyatt Childers <wchilders@nearce.com>
2015-06-06 22:14:14 -0400

    Dirty Skree is now known as FML Skree

--- src/main/java/com/skelril/skree/FMLSkree.java
+++ src/main/java/com/skelril/skree/FMLSkree.java
@@ -14,8 +14,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
  * differs from Sponge's InitializationEVent. Once this discrepancy is
  * resolved, the "Dirty Skree" mod can be removed from the project.
  */
-@Mod(modid = "dirtyskree", version = "1.0", name = "Dirty Skree")
-public class DirtySkree {
+@Mod(modid = "FMLSkree", version = "1.0", name = "FML Skree")
+public class FMLSkree {
     @Mod.EventHandler
     public void onInit(FMLInitializationEvent event) {
         SkreePlugin.customItemSystem.init();
