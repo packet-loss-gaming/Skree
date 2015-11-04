fa9ec736d803b9d643ab55b83e01073539566255
Wyatt Childers <wchilders@nearce.com>
2015-11-03 22:14:05 -0500

    Implemented the Life Leach Charm

--- src/main/java/com/skelril/skree/content/registry/charm/melee/LifeLeachCharm.java
+++ src/main/java/com/skelril/skree/content/registry/charm/melee/LifeLeachCharm.java
@@ -6,31 +6,46 @@
 
 package com.skelril.skree.content.registry.charm.melee;
 
-import com.google.common.base.Optional;
 import com.skelril.skree.content.registry.charm.AbstractCharm;
 import com.skelril.skree.content.registry.charm.CharmTools;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.manipulator.mutable.entity.HealthData;
 import org.spongepowered.api.entity.ArmorEquipable;
 import org.spongepowered.api.entity.Entity;
-import org.spongepowered.api.event.Subscribe;
-import org.spongepowered.api.event.entity.EntityInteractEntityEvent;
+import org.spongepowered.api.entity.living.Living;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.cause.Cause;
+import org.spongepowered.api.event.entity.InteractEntityEvent;
 import org.spongepowered.api.item.inventory.ItemStack;
+
+import java.util.Optional;
 
 public class LifeLeachCharm extends AbstractCharm {
     public LifeLeachCharm() {
         super(0, "life_leech", 3);
     }
 
-    @Subscribe
-    public void onEntityInteract(EntityInteractEntityEvent event) {
-        Entity entity = event.getEntity();
-        if (entity instanceof ArmorEquipable) {
-            Optional<ItemStack> held = ((ArmorEquipable) entity).getItemInHand();
-            if (held.isPresent()) {
-                int level = CharmTools.getLevel(held.get(), this);
-                if (level > 0) {
-
+    @Listener
+    public void onEntityInteract(InteractEntityEvent event) {
+        Entity target = event.getTargetEntity();
+        Optional<ArmorEquipable> optHolder = event.getCause().first(ArmorEquipable.class);
+        if (optHolder.isPresent() && target instanceof Living) {
+            ArmorEquipable holder = optHolder.get();
+            Optional<ItemStack> optHeldItem = holder.getItemInHand();
+            if (optHeldItem.isPresent() && holder instanceof Living) {
+                ItemStack heldItem = optHeldItem.get();
+                Optional<Integer> optCharmLevel = CharmTools.getLevel(heldItem, this);
+                if (optCharmLevel.isPresent()) {
+                    int charmLevel = optCharmLevel.get();
+                    process((Living) holder, (Living) target, heldItem, charmLevel);
                 }
             }
         }
+    }
+
+    private void process(Living attacker, Living defender, ItemStack stack, int power) {
+        defender.damage(power, Cause.of(this, stack, attacker));
+        HealthData data = attacker.getHealthData();
+        attacker.offer(Keys.HEALTH, Math.max(data.health().get() + power, data.maxHealth().get()));
     }
 }
