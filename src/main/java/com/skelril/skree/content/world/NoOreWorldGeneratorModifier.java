841f2ca555ad1e7133fdae53a4aa8984d47b09ca
Wyatt Childers <wchilders@nearce.com>
2015-12-28 00:45:35 -0500

    Fixed the NoOreWorldGeneratorModifier to remove ores from the build world

--- src/main/java/com/skelril/skree/content/world/NoOreWorldGeneratorModifier.java
+++ src/main/java/com/skelril/skree/content/world/NoOreWorldGeneratorModifier.java
@@ -6,9 +6,11 @@
 
 package com.skelril.skree.content.world;
 
+import com.skelril.skree.SkreePlugin;
 import org.spongepowered.api.data.DataContainer;
 import org.spongepowered.api.world.WorldCreationSettings;
-import org.spongepowered.api.world.gen.Populator;
+import org.spongepowered.api.world.biome.BiomeGenerationSettings;
+import org.spongepowered.api.world.biome.BiomeType;
 import org.spongepowered.api.world.gen.WorldGenerator;
 import org.spongepowered.api.world.gen.WorldGeneratorModifier;
 import org.spongepowered.api.world.gen.populator.Ore;
@@ -18,8 +20,11 @@ import java.util.List;
 public class NoOreWorldGeneratorModifier implements WorldGeneratorModifier {
     @Override
     public void modifyWorldGenerator(WorldCreationSettings world, DataContainer settings, WorldGenerator worldGenerator) {
-        List<Populator> orePopulators = worldGenerator.getPopulators(Ore.class);
-        worldGenerator.getPopulators().removeAll(orePopulators);
+        for (BiomeType biomeType : SkreePlugin.inst().getGame().getRegistry().getAllOf(BiomeType.class)) {
+            BiomeGenerationSettings biomeData = worldGenerator.getBiomeSettings(biomeType);
+            List<Ore> populators = biomeData.getPopulators(Ore.class);
+            biomeData.getPopulators().removeAll(populators);
+        }
     }
 
     @Override
