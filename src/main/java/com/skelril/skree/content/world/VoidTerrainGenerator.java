854c6cfbccf355bf1474a37da908ad27aba735a3
Wyatt Childers <wchilders@nearce.com>
2016-02-11 18:55:05 -0500

    Revert changes to the Void Terrain Generator

--- src/main/java/com/skelril/skree/content/world/VoidTerrainGenerator.java
+++ src/main/java/com/skelril/skree/content/world/VoidTerrainGenerator.java
@@ -6,8 +6,6 @@
 
 package com.skelril.skree.content.world;
 
-import com.flowpowered.math.vector.Vector3i;
-import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.world.World;
 import org.spongepowered.api.world.extent.ImmutableBiomeArea;
 import org.spongepowered.api.world.extent.MutableBlockVolume;
@@ -16,14 +14,6 @@ import org.spongepowered.api.world.gen.GenerationPopulator;
 public class VoidTerrainGenerator implements GenerationPopulator {
     @Override
     public void populate(World world, MutableBlockVolume buffer, ImmutableBiomeArea biomes) {
-        Vector3i min = buffer.getBlockMin();
-        Vector3i max = buffer.getBlockMax();
-        for (int x = min.getX(); x <= max.getX(); ++x) {
-            for (int z = min.getZ(); z <= max.getZ(); ++z) {
-                buffer.setBlockType(x, 0, z, BlockTypes.BEDROCK);
-                buffer.setBlockType(x, 1, z, BlockTypes.STONE);
-                buffer.setBlockType(x, 5, z, BlockTypes.STONE);
-            }
-        }
+
     }
 }
