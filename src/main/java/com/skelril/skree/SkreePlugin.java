05ae3406604d7e7a07aaf452286edf5a1483be18
Wyatt Childers <wchilders@nearce.com>
2016-05-11 17:58:57 -0400

    Added a command to view server memory

--- src/main/java/com/skelril/skree/SkreePlugin.java
+++ src/main/java/com/skelril/skree/SkreePlugin.java
@@ -14,6 +14,7 @@ import com.skelril.skree.system.aid.AidSystem;
 import com.skelril.skree.system.arrowfishing.ArrowFishingSystem;
 import com.skelril.skree.system.database.DatabaseSystem;
 import com.skelril.skree.system.dropclear.DropClearSystem;
+import com.skelril.skree.system.maintenance.MaintenanceSystem;
 import com.skelril.skree.system.market.MarketSystem;
 import com.skelril.skree.system.modifier.ModifierSystem;
 import com.skelril.skree.system.playerstate.PlayerStateSystem;
@@ -75,6 +76,7 @@ public class SkreePlugin {
                 CustomRegisterySystem.class,
                 DatabaseSystem.class,
                 DropClearSystem.class,
+                MaintenanceSystem.class,
                 MarketSystem.class,
                 ModifierSystem.class,
                 PlayerStateSystem.class,
---
+++ src/main/java/com/skelril/skree/content/maintenance/ServerMemoryCommand.java
@@ -0,0 +1,39 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.skelril.skree.content.maintenance;
+
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+
+public class ServerMemoryCommand implements CommandExecutor {
+    @Override
+    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+        int mb = 1024 * 1024;
+
+        Runtime runtime = Runtime.getRuntime();
+
+        src.sendMessage(Text.of(TextColors.YELLOW, "Used memory: ", (runtime.totalMemory() - runtime.freeMemory()) / mb));
+        src.sendMessage(Text.of(TextColors.YELLOW, "Free memory: ", runtime.freeMemory() / mb));
+        src.sendMessage(Text.of(TextColors.YELLOW, "Total memory: ", runtime.totalMemory() / mb));
+        src.sendMessage(Text.of(TextColors.YELLOW, "Max memory: ", runtime.maxMemory() / mb));
+
+        return CommandResult.success();
+    }
+
+    public static CommandSpec aquireSpec() {
+        return CommandSpec.builder()
+                .description(Text.of("View memory information"))
+                .executor(new ServerMemoryCommand())
+                .build();
+    }
+}
