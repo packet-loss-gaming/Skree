27d7507467a6f8473b9ba1d59e22ea9211f09405
Wyatt Childers <wchilders@nearce.com>
2016-05-17 01:04:25 -0400

    Fixed a misspelled word in my previous commit

--- src/main/java/com/skelril/skree/content/zone/group/jungleraid/JungleRaidEffectListener.java
+++ src/main/java/com/skelril/skree/content/zone/group/jungleraid/JungleRaidEffectListener.java
@@ -170,7 +170,7 @@ public class JungleRaidEffectListener {
 
             Optional<JungleRaidInstance> optInst = manager.getApplicableZone(entity);
             if (optInst.isPresent()) {
-                // The Player cause should not be necissary, workaround for SpongeCommon/729
+                // The Player cause should not be necessary, workaround for SpongeCommon/729
                 Optional<EntitySpawnCause> optSpawnCause = event.getCause().first(EntitySpawnCause.class);
                 Optional<Player> optPlayerCause = event.getCause().first(Player.class);
                 if (!optSpawnCause.isPresent() && !optPlayerCause.isPresent()) {
