a2edafec92ad7ac747da28d7b0ffabb9591915c2
Wyatt Childers <wchilders@nearce.com>
2016-08-14 00:29:57 -0400

    Changed to rapidly damaging mobs

--- src/main/java/com/skelril/skree/content/mobdensity/MobDensityListener.java
+++ src/main/java/com/skelril/skree/content/mobdensity/MobDensityListener.java
@@ -6,13 +6,10 @@
 
 package com.skelril.skree.content.mobdensity;
 
-import com.google.common.collect.Lists;
-import org.spongepowered.api.data.key.Keys;
-import org.spongepowered.api.effect.potion.PotionEffect;
-import org.spongepowered.api.effect.potion.PotionEffectTypes;
 import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.living.animal.Animal;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.cause.entity.damage.source.DamageSources;
 import org.spongepowered.api.event.entity.CollideEntityEvent;
 
 import java.util.List;
@@ -21,18 +18,9 @@ import java.util.stream.Collectors;
 public class MobDensityListener {
     @Listener
     public void onEntityCollide(CollideEntityEvent event) {
-        PotionEffect witherEffect = PotionEffect.builder()
-                .potionType(PotionEffectTypes.WITHER)
-                .amplifier(0)
-                .duration(20 * 3)
-                .particles(true)
-                .build();
-
         List<Entity> entities = event.getEntities().stream().filter(e -> e instanceof Animal).collect(Collectors.toList());
         if (entities.size() > 5) {
-            entities.forEach(e -> {
-                e.offer(Keys.POTION_EFFECTS, Lists.newArrayList(witherEffect));
-            });
+            entities.forEach(e -> { e.damage(5, DamageSources.GENERIC); });
         }
     }
 }
