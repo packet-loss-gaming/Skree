f2ae641f2fcd22ec15137feff98ae28343055842
Wyatt Childers <wchilders@nearce.com>
2016-08-14 00:24:56 -0400

    Changed to wither based mob density killing

--- src/main/java/com/skelril/skree/content/mobdensity/MobDensityListener.java
+++ src/main/java/com/skelril/skree/content/mobdensity/MobDensityListener.java
@@ -6,7 +6,10 @@
 
 package com.skelril.skree.content.mobdensity;
 
+import com.google.common.collect.Lists;
 import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.effect.potion.PotionEffect;
+import org.spongepowered.api.effect.potion.PotionEffectTypes;
 import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.living.animal.Animal;
 import org.spongepowered.api.event.Listener;
@@ -18,10 +21,17 @@ import java.util.stream.Collectors;
 public class MobDensityListener {
     @Listener
     public void onEntityCollide(CollideEntityEvent event) {
+        PotionEffect witherEffect = PotionEffect.builder()
+                .potionType(PotionEffectTypes.WITHER)
+                .amplifier(0)
+                .duration(20 * 3)
+                .particles(true)
+                .build();
+
         List<Entity> entities = event.getEntities().stream().filter(e -> e instanceof Animal).collect(Collectors.toList());
         if (entities.size() > 5) {
             entities.forEach(e -> {
-                e.offer(Keys.FIRE_TICKS, 20 * 20);
+                e.offer(Keys.POTION_EFFECTS, Lists.newArrayList(witherEffect));
             });
         }
     }
