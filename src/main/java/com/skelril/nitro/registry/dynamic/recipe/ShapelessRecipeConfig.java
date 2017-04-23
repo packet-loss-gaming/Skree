0c246386cacb9078ab5276227f5dd8df38e3ca84
Wyatt Childers <wchilders@nearce.com>
2017-04-23 00:19:48 -0400

    Added some initial, presently unused code for dynamic recipe registration to Nitro

---
+++ src/main/java/com/skelril/nitro/registry/dynamic/recipe/ShapelessRecipeConfig.java
@@ -0,0 +1,23 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.skelril.nitro.registry.dynamic.recipe;
+
+import com.skelril.nitro.registry.dynamic.ItemStackConfig;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+import java.util.List;
+
+public class ShapelessRecipeConfig extends CraftingRecipeConfig {
+    List<ItemStackConfig> requiredItemStacks;
+
+    public void registerRecipie() {
+        GameRegistry.addRecipe(
+                craftedItem.toNSMStack(),
+                requiredItemStacks.stream().map(ItemStackConfig::toNSMStack).toArray()
+        );
+    }
+}
