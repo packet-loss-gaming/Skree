63c6bb3bc24989c30014e7ad76672c35b4d664cf
Wyatt Childers <wchilders@nearce.com>
2017-04-27 21:22:31 -0400

    Added a method to the special attack interface to generate a damage source

--- src/main/java/com/skelril/nitro/registry/dynamic/item/ability/SpecialAttack.java
+++ src/main/java/com/skelril/nitro/registry/dynamic/item/ability/SpecialAttack.java
@@ -8,10 +8,17 @@ package com.skelril.nitro.registry.dynamic.item.ability;
 
 import org.spongepowered.api.entity.living.Living;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.cause.entity.damage.DamageTypes;
+import org.spongepowered.api.event.cause.entity.damage.source.DamageSource;
+import org.spongepowered.api.event.cause.entity.damage.source.EntityDamageSource;
 import org.spongepowered.api.text.Text;
 
 public interface SpecialAttack extends Ability {
     void run(Living owner, Living target);
+
+    default DamageSource damageSource(Living owner) {
+        return EntityDamageSource.builder().entity(owner).type(DamageTypes.ATTACK).build();
+    }
 
     default void notify(Living living, Text message) {
         if (living instanceof Player) {
