162db02c0e296d88b20851f9eede6a49f3c0d6bf
Wyatt Childers <wchilders@nearce.com>
2017-04-27 21:25:38 -0400

    Register all newly ported abilities with the Skree ability registry

--- src/main/java/com/skelril/skree/content/registry/item/ability/SkreeAbilityRegistry.java
+++ src/main/java/com/skelril/skree/content/registry/item/ability/SkreeAbilityRegistry.java
@@ -8,7 +8,7 @@ package com.skelril.skree.content.registry.item.ability;
 
 import com.skelril.nitro.registry.dynamic.item.ability.Ability;
 import com.skelril.nitro.registry.dynamic.item.ability.AbilityRegistry;
-import com.skelril.skree.content.registry.item.ability.combat.DoomBlade;
+import com.skelril.skree.content.registry.item.ability.combat.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,7 +17,12 @@ public class SkreeAbilityRegistry implements AbilityRegistry {
     private Map<String, Class<? extends Ability>> registry = new HashMap<>();
 
     public SkreeAbilityRegistry() {
+        registerAbility("agility", Agility.class);
         registerAbility("doom_blade", DoomBlade.class);
+        registerAbility("evil_focus", EvilFocus.class);
+        registerAbility("healing_light", HealingLight.class);
+        registerAbility("life_leech", LifeLeech.class);
+        registerAbility("regen", Regen.class);
     }
 
     @Override
