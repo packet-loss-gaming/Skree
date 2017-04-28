8e080f15861c176e7d4fe05f350cf9d20a8a8580
Wyatt Childers <wchilders@nearce.com>
2017-04-27 21:24:37 -0400

    Ported Evil Focus

---
+++ src/main/java/com/skelril/skree/content/registry/item/ability/combat/EvilFocus.java
@@ -0,0 +1,44 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.skelril.skree.content.registry.item.ability.combat;
+
+import com.skelril.nitro.entity.EntityHealthUtil;
+import com.skelril.nitro.registry.dynamic.item.ability.SpecialAttack;
+import org.spongepowered.api.data.manipulator.mutable.PotionEffectData;
+import org.spongepowered.api.effect.potion.PotionEffect;
+import org.spongepowered.api.effect.potion.PotionEffectTypes;
+import org.spongepowered.api.effect.sound.SoundTypes;
+import org.spongepowered.api.entity.living.Living;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+
+import java.util.Optional;
+
+public class EvilFocus implements SpecialAttack {
+    @Override
+    public void run(Living owner, Living target) {
+        Optional<PotionEffectData> optPotionEffectData = target.getOrCreate(PotionEffectData.class);
+        if (!optPotionEffectData.isPresent()) {
+            return;
+        }
+
+        PotionEffectData potionEffectData = optPotionEffectData.get();
+
+        int duration = (int) (EntityHealthUtil.getHealth(target) * 10);
+        potionEffectData.addElement(PotionEffect.of(PotionEffectTypes.SLOWNESS, 9, duration));
+        if (target instanceof Player) {
+            potionEffectData.addElement(PotionEffect.of(PotionEffectTypes.BLINDNESS, 0, 20 * 4));
+        }
+
+        target.offer(potionEffectData);
+
+        target.getWorld().playSound(SoundTypes.ENTITY_GHAST_SCREAM, target.getLocation().getPosition(), 1, .02F);
+
+        notify(owner, Text.of(TextColors.YELLOW, "Your weapon traps your foe in their own sins."));
+    }
+}
