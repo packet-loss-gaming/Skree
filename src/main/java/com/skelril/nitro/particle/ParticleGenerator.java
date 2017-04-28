baf2a0d77cd32d594ac7582cd2ccb46cb290bcfa
Wyatt Childers <wchilders@nearce.com>
2017-04-27 21:23:39 -0400

    Added a particle generator for generating particle effects

---
+++ src/main/java/com/skelril/nitro/particle/ParticleGenerator.java
@@ -0,0 +1,28 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.skelril.nitro.particle;
+
+import com.skelril.nitro.probability.Probability;
+import org.spongepowered.api.effect.particle.ParticleEffect;
+import org.spongepowered.api.effect.particle.ParticleTypes;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+
+
+public class ParticleGenerator {
+    private static final ParticleEffect FLAME = ParticleEffect.builder().type(ParticleTypes.FLAME).quantity(1).build();
+
+    public static void mobSpawnerFlames(Location<World> location, int intensity) {
+        for (int i = 0; i < 20 * intensity; ++i ) {
+            location.getExtent().spawnParticles(FLAME, location.getPosition().add(
+                    Probability.getRangedRandom(-.5, .5),
+                    Probability.getRangedRandom(0.0, 1.0),
+                    Probability.getRangedRandom(-.5, .5)
+            ));
+        }
+    }
+}
