0a5b9fab025e085a0521448b8b4a5491feb82981
Wyatt Childers <wchilders@nearce.com>
2017-05-29 14:55:14 -0400

    Change the blaze sounds long used by wilderness item pools into a short harp based sound

--- src/main/java/com/skelril/skree/content/world/wilderness/WildernessDropPool.java
+++ src/main/java/com/skelril/skree/content/world/wilderness/WildernessDropPool.java
@@ -9,9 +9,12 @@ package com.skelril.skree.content.world.wilderness;
 import com.flowpowered.math.vector.Vector3d;
 import com.skelril.nitro.item.ItemDropper;
 import com.skelril.nitro.time.IntegratedRunnable;
+import com.skelril.nitro.time.TimedRunnable;
+import com.skelril.skree.SkreePlugin;
 import org.spongepowered.api.effect.sound.SoundTypes;
 import org.spongepowered.api.event.cause.entity.spawn.SpawnTypes;
 import org.spongepowered.api.item.inventory.ItemStackSnapshot;
+import org.spongepowered.api.scheduler.Task;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
@@ -42,21 +45,39 @@ public class WildernessDropPool implements IntegratedRunnable {
     public boolean run(int timesL) {
         dropper.dropStackSnapshots(itemStackProvider.get(), SpawnTypes.DROPPED_ITEM);
 
-        getExtent().playSound(
-                SoundTypes.BLOCK_DISPENSER_DISPENSE,
-                getPos(),
-                Math.min(
-                        1,
-                        (((float) timesL / times) * .6F) + ((float) 1 / times)
-                ),
-                1
-        );
+        float volumeLevel = Math.min(1, (((float) timesL / times) * .6F) + ((float) 1 / times));
+
+        IntegratedRunnable music = new IntegratedRunnable() {
+            @Override
+            public boolean run(int musicPitchT) {
+                getExtent().playSound(
+                        SoundTypes.BLOCK_NOTE_HARP,
+                        getPos(),
+                        volumeLevel,
+                        (musicPitchT / 3) + 1
+                );
+                return true;
+            }
+
+            @Override
+            public void end() {
+            }
+        };
+
+        TimedRunnable<IntegratedRunnable> runnable = new TimedRunnable<>(music, 3);
+        Task task = Task.builder().execute(runnable).intervalTicks(3).submit(SkreePlugin.inst());
+        runnable.setTask(task);
 
         return true;
     }
 
     @Override
     public void end() {
-
+        getExtent().playSound(
+                SoundTypes.BLOCK_NOTE_HARP,
+                getPos(),
+                1,
+                0
+        );
     }
 }
